784b4782f581be7820f54a361007798da0be714a
Philip Potter <philip.g.potter@gmail.com>
2021-04-12 15:45:37 +0100

    refactor pkcs#7 padding

--- padding.go
+++ padding.go
@@ -1,18 +1,17 @@
 package cryptopals
 
-func Pkcs7pad(buf []byte, size int) []byte {
-	if size >= 256 {
+// Pkcs7pad pads buf with PKCS#7 padding to the given block size.  buf
+// will be append()ed to to make this happen, so treat it like
+// append().
+func Pkcs7pad(buf []byte, blockSize int) []byte {
+	if blockSize >= 256 {
 		panic("block size too big")
 	}
-	outsize := ((len(buf) / size) + 1) * size
-	out := make([]byte, outsize)
-	copy(out, buf)
-	paddingBytes := outsize - len(buf)
-	if paddingBytes <= 0 || paddingBytes > size {
-		panic("logic error")
+	lastBlockBytes := len(buf) % blockSize
+	paddingBytes := blockSize - lastBlockBytes
+
+	for i := 0; i < paddingBytes; i++ {
+		buf = append(buf, byte(paddingBytes))
 	}
-	for i := len(buf); i < outsize; i++ {
-		out[i] = byte(paddingBytes)
-	}
-	return out
+	return buf
 }
